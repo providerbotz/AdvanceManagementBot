529ad163658f74c58178e7efd78256fb282a1d34
˹  ᴘʀᴏᴠɪᴅᴇʀʙᴏᴛᴢ  ˼ <animeokids@gmail.com>
2026-01-11 15:30:58 +0530

    Update x-WORKER-x.py

--- Provider/modules/x-WORKER-x.py
+++ Provider/modules/x-WORKER-x.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
-from Midukki.midukki import Midukki_RoboT
-from Midukki.modules.auto_filters import auto_filters, send_for_index
-from Midukki.modules.manual_filters import manual_filters
-from Midukki.functions.settings import get_settings
-from Midukki.database import db
+from Provider.provider import AdvanceManagementBot
+from Provider.modules.auto_filters import auto_filters, send_for_index
+from Provider.modules.manual_filters import manual_filters
+from Provider.functions.settings import get_settings
+from Provider.database import db
 
 
-@Midukki_RoboT.on_message(filters.group & filters.text & filters.incoming)
+@AdvanceManagementBot.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
 
     k = await manual_filters(client, message)
@@ -15,6 +15,6 @@ async def give_filter(client, message):
         if settings["autofilter"]:
             await auto_filters(client, message)
 
-@Midukki_RoboT.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
+@AdvanceManagementBot.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
 async def start_for_index(client, message):
     await send_for_index(client, message)
